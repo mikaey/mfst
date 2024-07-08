a6d242c0e26bfa953578d353111299a8802d5455
Matt Cole <mikaey@bahjeez.com>
2024-07-07 23:12:25 -0500

    Spacing fix

--- block_size_test.h
+++ block_size_test.h
@@ -3,16 +3,16 @@
 
 /**
  * Probe the device to determine the optimal size of write requests.
- * 
+ *
  * The test involves writing 256MB of data to the device in a sequential
  * fashion using various block sizes from 512 bytes to 64MB.  Block sizes that
  * are smaller than the device's optimal block size (as returned by the fstat()
  * call) and bigger than the maximum number of sectors per request (as returned
  * by ioctl(BLKSECTGET) call) are excluded.  The amount of time it takes to
  * write the data using each block size is measured.
- * 
+ *
  * @param fd  The file descriptor of the device being measured.
- * 
+ *
  * @returns The block size which the device was able to write the quickest, in
  *          bytes.
  */
